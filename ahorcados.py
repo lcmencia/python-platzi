7fdf337a21d8bf1a846fd87a76338efe8c7626e2
Lcmencia <luiscarlos@lcmencia.com>
2019-12-19 22:40:02 -0300

    Hangman's logic

--- ahorcados.py
+++ ahorcados.py
@@ -98,6 +98,31 @@ def run():
         display_board(hidden_word, tries)
         current_letter = str(input('Escoge una letra: '))
 
+        letter_indexes = []
+        for idx in range(len(word)):
+            if word[idx] == current_letter:
+                letter_indexes.append(idx)
+        if len(letter_indexes) == 0:
+            tries += 1
+
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print('')
+                print('Perdiste! La palabra correcta era {}'.format(word))
+                break
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
+
+            letter_indexes = []
+
+        try:
+            hidden_word.index('-')
+        except ValueError:
+            print('')
+            print('Felicidades! Ganaste. La palabra es: {}'. format(word))
+            break
+
 
 if __name__ == '__main__':
     print('B I E N V E N I D O  A  A H O R C A D O S')
